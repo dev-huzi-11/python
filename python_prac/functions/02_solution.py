7ff313ea665f72ad105943d365811baa9655d3d9
Huzaifa <162676983+dev-huzi-11@users.noreply.github.com>
2025-03-20 00:03:27 +0500

    solution 02

---
+++ python_prac/functions/02_solution.py
@@ -0,0 +1,8 @@
+# ************************************* SUM OF TWO NUMBERS *************************************
+
+# Function to calculate the sum of two numbers
+def sum(num1, num2):
+    return num1 + num2  # Returns the sum of num1 and num2
+
+# Calling the function and printing the result
+print(sum(4, 10))  # Output: 14
