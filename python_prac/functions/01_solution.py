911fbd2aad66276177955c096f58ebb0bb6ef166
Huzaifa <162676983+dev-huzi-11@users.noreply.github.com>
2025-03-19 23:54:36 +0500

    solution 01

---
+++ python_prac/functions/01_solution.py
@@ -0,0 +1,8 @@
+# ************************************* CALCULATING SQUARE *************************************
+
+# Function to calculate the square of a given number
+def square(square_number):
+    return square_number ** 2  # Returns the square of the input number
+
+# Calling the function and printing the result
+print(square(5))  # Output: 25
