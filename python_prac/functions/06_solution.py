29a6d049a824996caa75da4f60655fcec7525a48
Huzaifa <162676983+dev-huzi-11@users.noreply.github.com>
2025-03-21 20:01:21 +0500

    solution 06

---
+++ python_prac/functions/06_solution.py
@@ -0,0 +1,7 @@
+# ************************************* LAMBDA FUNCTION *************************************
+
+# Define a lambda function to calculate the cube of a number
+cube = lambda num: num ** 3  
+
+# Call the lambda function with the value 5 and print the result
+print(cube(5))  # Output: 125
